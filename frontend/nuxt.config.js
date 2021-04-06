f8e72e4f2ce6cb24fe96613a7c8438f5f41eb13e
Min ho Kim <minho42@gmail.com>
2021-04-06 13:27:00 +1000

    title, description

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -7,14 +7,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "frontend",
+    title: "Medical abbreviations",
     htmlAttrs: {
       lang: "en"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      {
+        hid: "description",
+        name: "description",
+        content: "Medical abbreviations"
+      }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
