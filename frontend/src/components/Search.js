cd10cc0d5feaa43dfaa234b0e2b8571e766e681d
Min ho Kim <minho42@gmail.com>
2021-05-21 02:47:46 +1000

    style

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -7,12 +7,12 @@ const Search = (props) => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col justify-center px-2 py-2">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search abbreviations"
-          className="pl-6 mx-4 pr-24 py-2 rounded-lg w-full border border-gray-300 focus:border-gray-800 bg-gray-100 focus:outline-none"
+          className="px-6 py-2 rounded-lg w-full border border-gray-300 focus:border-gray-800 bg-gray-100 focus:outline-none"
           autoFocus
           onChange={handleChange}
         />
