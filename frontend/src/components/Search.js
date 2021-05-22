0b4f4cbef94dd1ee26282167617c2d33f728f4b7
Min ho Kim <minho42@gmail.com>
2021-05-23 00:15:20 +1000

    add clearInput

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,4 +1,5 @@
 const Search = (props) => {
+  const searchInput = document.querySelector("input");
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -6,16 +7,48 @@ const Search = (props) => {
     props.onSearchChange(e.target.value.toLowerCase().trim());
   };
 
+  const clearInput = () => {
+    searchInput.value = "";
+    searchInput.focus();
+    props.onSearchChange("");
+  };
+
   return (
-    <div className="flex flex-col justify-center px-4 py-2">
+    <div className="flex flex-col justify-center px-4 py-2 relative">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search abbreviations"
-          className="px-6 py-2 rounded-lg w-full border border-gray-300 focus:border-gray-800 bg-gray-100 focus:outline-none"
+          className="pl-6 pr-16 py-2 rounded-lg w-full border border-gray-300 focus:border-gray-800 bg-gray-100 focus:outline-none"
           autoFocus
+          // value={value}
           onChange={handleChange}
         />
+        {searchInput.value.trim().length > 0 ? (
+          <div className="absolute inset-y-0 right-10 flex items-center">
+            <button
+              onClick={clearInput}
+              className="h-full px-3 text-gray-500 hover:text-gray-800 focus:outline-none"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
       </form>
     </div>
   );
