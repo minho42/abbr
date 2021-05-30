da0c4e24b45696b07962da3229bc798b05df8cfe
Min ho Kim <minho42@gmail.com>
2021-05-30 23:22:03 +1000

    remove comment

--- settings/base.py
+++ settings/base.py
@@ -61,7 +61,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                # `allauth` needs this from django
                 "django.template.context_processors.request",
                 # this allows navbar.html to access {{ MEDIA_URL }}
                 "django.template.context_processors.media",
