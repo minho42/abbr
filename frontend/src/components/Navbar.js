504f66b13a34e65e7f2076292a5ea9d2d6e9ea55
Min ho Kim <minho42@gmail.com>
2022-02-26 23:07:58 +1100

    style: removed shadow

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-white border-b border-gray-300 shadow-sm">
+      <nav className="bg-white border-b border-gray-300">
         <div className="flex items-center justify-center pl-3 py-2 h-12">
           <div className="flex items-center">
             <div className="px-1 py-1 font-semibold">Medical Abbreviations</div>
