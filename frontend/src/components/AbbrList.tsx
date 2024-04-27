6a466b50540078cbc7b0ab1a06468cfc36f14a85
Min ho Kim <minho42@gmail.com>
2024-04-27 23:39:57 +1000

    move AbbrItem into AbbrList

--- frontend/src/components/AbbrList.tsx
+++ frontend/src/components/AbbrList.tsx
@@ -1,44 +1,52 @@
-import AbbrItem from "./AbbrItem";
-
-export interface IData {
-  id: number;
-  name: string;
-  description: string;
+type Data = {
+  id: number
+  name: string
+  description: string
 }
 
-const AbbrList: React.FC<{ query: string; data: IData[] }> = ({ query, data }) => {
+const AbbrItem: React.FC<{ data: Data }> = ({ data }) => {
+  const { name, description } = data
+  return (
+    <tr className="border-b border-gray-200">
+      <td className="text-center py-2">{name}</td>
+      <td className="pl-2">{description}</td>
+    </tr>
+  )
+}
+
+const AbbrList: React.FC<{ query: string; data: Data[] }> = ({ query, data }) => {
   const searchResult = () => {
-    let result: IData[] = [];
+    let result: Data[] = []
 
     if (query.length < 1) {
-      return [];
+      return []
     }
 
     result.push(
       ...data.filter((item) => {
-        return item.name.toLowerCase() === query;
+        return item.name.toLowerCase() === query
       })
-    );
+    )
     result.push(
       ...data.filter((item) => {
-        return item.name.toLowerCase().startsWith(query);
+        return item.name.toLowerCase().startsWith(query)
       })
-    );
+    )
     result.push(
       ...data.filter((item) => {
-        return item.name.toLowerCase().includes(query);
+        return item.name.toLowerCase().includes(query)
       })
-    );
+    )
 
     if (query.length > 2) {
       result.push(
         ...data.filter((item) => {
-          return item.description.toLowerCase().includes(query);
+          return item.description.toLowerCase().includes(query)
         })
-      );
+      )
     }
-    return [...new Set(result)];
-  };
+    return [...new Set(result)]
+  }
 
   return (
     <div className="flex flex-col justify-center mx-2 sm:mx-6">
@@ -51,12 +59,12 @@ const AbbrList: React.FC<{ query: string; data: IData[] }> = ({ query, data }) =
         </thead>
         <tbody>
           {searchResult().map((row) => {
-            return <AbbrItem key={row.id} data={row} />;
+            return <AbbrItem key={row.id} data={row} />
           })}
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default AbbrList;
+export default AbbrList
